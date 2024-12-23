f1dffa75825db973049bff1399e078afdd081274
Taj <t4jt1j@gmail.com>
2024-12-23 09:52:15 -0600

    Add basic code for computer choice

--- rps.js
+++ rps.js
@@ -1,1 +1,12 @@
-console.log("Wassah doo?");
+function getComputerChoice() {
+    computerChoice = Math.random();
+    if (computerChoice < 0.333) {
+        return "rock";
+    } else if (computerChoice <= 0.666) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
